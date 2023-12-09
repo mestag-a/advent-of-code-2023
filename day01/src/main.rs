1924ff9592a87668256cc6afec29b24710f1d82e
Alexis MESTAG <alexis.mestag@protonmail.com>
2023-12-10 00:13:40 +0100

    Day 01, part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::min;
 
 fn part1(s: &str) -> u32 {
     s.lines()
@@ -15,8 +16,32 @@ fn part1(s: &str) -> u32 {
     .sum()
 }
 
+const SPELLED_OUT_DIGITS: &[&str] = &["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+fn spelled_out_to_digit(s: &str) -> Option<u32> {
+    for (value, &digit) in SPELLED_OUT_DIGITS.iter().enumerate() {
+        if digit == &s[..min(s.len(), digit.len())] {
+            return Some(value as u32)
+        }
+    }
+    None
+}
+
 fn part2(s: &str) -> u32 {
-    0
+    s.lines()
+        .map(|s| -> u32 {
+            let mut numbers = s
+                .chars()
+                .enumerate()
+                .filter_map(|(idx, c)| {
+                    c.to_digit(10)
+                        .or_else(|| { spelled_out_to_digit(&s[idx..]) })
+                })
+                .peekable();
+
+            numbers.peek().unwrap() * 10 + numbers.last().unwrap()
+        })
+        .sum()
 }
 
 fn main() {
@@ -40,5 +65,25 @@ a1b2c3d4e5f
 treb7uchet";
 
         assert_eq!(part1(s), 142);
+    }
+
+    #[test]
+    fn test_spelled_out_digits() {
+        assert_eq!(spelled_out_to_digit("one"), Some(1));
+        assert_eq!(spelled_out_to_digit("oneaaaaa"), Some(1));
+        assert_eq!(spelled_out_to_digit("eightZZ"), Some(8));
+    }
+
+    #[test]
+    fn test_part2() {
+        let s = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+        assert_eq!(part2(s), 281);
     }
 }
